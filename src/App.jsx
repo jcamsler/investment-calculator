17c3f2ca83946dfce95d2ec41663badd3f26b9ef
jcamsler <jcamsler@gmail.com>
2024-02-22 15:38:20 +0100

    finalizing app

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,35 @@
 import { useState } from "react";
-import { calculateInvestmentResults, formatter } from "./util/investment"
+
 import Header from "./components/Header";
 import UserInput from "./components/UserInput";
+import Result from "./components/Result";
 
 function App() {
+
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn:6,
+    duration : 10,
+  });
+
+  
+  function handleChange(inputIdentifier, newValue){
+    setUserInput(prevUserInput => {
+        return {
+            ...prevUserInput,
+            [inputIdentifier]:+newValue
+        }
+    });
+}
+
 
   return ( 
       <>
         <Header/>
-        <UserInput/>         
+        <UserInput userInput={userInput} onChange={handleChange}/>
+        {/* Result go here */}         
+        <Result input={userInput}/>
       </>      
     )
   }
